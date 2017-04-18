109d3b9dd9b87240cad8906398b26b92eeda1836
Rusta <rustauk@gmail.com>
2017-04-18 23:18:43 +0100

    going public

--- gatsby-node.js
+++
@@ -1,9 +0,0 @@
-exports.modifyWebpackConfig = function(config, stage) {
-  if (stage !== `develop-html`) {
-    config._config.resolve.alias = {
-      react: `preact-compat`,
-      'react-dom': `preact-compat`
-    };
-  }
-  return config;
-}
--- pages/about.md
+++
@@ -1,9 +0,0 @@
----
-title: About
-description: Gatsby Starter Kit
-keywords: gatsby, starter
----
-
-## About Heading
-
-Click the header to go back to the home page
